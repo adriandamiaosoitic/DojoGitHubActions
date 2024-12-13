71d93eb9e94eae49aa517f909e50ce9e1a1d8f1e
Adrian Damião <adrian.damiao@soitic.com>
2024-12-13 17:24:23 -0300

    Update UnitTest1.cs

--- BuildETestes/UnitTest1.cs
+++ BuildETestes/UnitTest1.cs
@@ -6,10 +6,7 @@ public class UnitTest1
     public void GaranteQueSomaFunciona()
     {
         var numero1 = 11;
-        var numero2 = 100;
-        var teste = "";
-        Assert.Equal(numero1 + numero2, Somar(numero1, numero2));
-    }
+        var numero2 = 
 
-    int Somar(int numero1, int numero2) => numero1 * numero2;
+    int Somar(int numero1, int numero2) => numero1 + numero2;
 }
